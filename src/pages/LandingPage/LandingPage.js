235f568b1199bd9a5f51503ce146d7735d747795
petermc87 <peter.mcgibney@gmail.com>
2023-01-25 16:10:17 -0500

    styled signup page, mobile responsive

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -6,7 +6,6 @@ import LoginForm from "../../components/LoginForm/LoginForm"
 export default function LandingPage(props){
     return(
         <main>
-            <h1>Auth Page</h1>
             <SignUpForm setUser={props.setUser}/>
             <LoginForm setUser={props.setUser}/>
         </main>
