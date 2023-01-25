d302e1edb922bde95cf4437169ba5da656171ae0
petermc87 <peter.mcgibney@gmail.com>
2023-01-25 09:27:20 -0500

    added likes routes and updated post model with likes array

---
+++ routes/api/likes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const { dataController, apiController} = require('../../controllers/api/likes')
+
+// GET /api/comments
+router.get('/', dataController.index, apiController.index)
+// DELETE /api/comments/:id
+router.delete('/:id', dataController.destroy, apiController.show)
+// CREATE /api/comments - adding the id of the blog to be updated
+router.post('/:id', dataController.create, apiController.show)
+
+//MAY NOT BE REQUIRED
+// // SHOW /api/comments/:id
+// router.get('/:id', dataController.show, apiController.show)
+
+module.exports = router
