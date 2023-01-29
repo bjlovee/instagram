b603320886c45b1835205d14f29d2a007d972e5b
petermc87 <peter.mcgibney@gmail.com>
2023-01-28 22:36:52 -0500

    added suggested profile section and media queries

---
+++ src/components/ProfilePostsIndex/ProfilePostsIndex.js
@@ -0,0 +1,3 @@
+export default function ProfilePostsIndex(){
+    return <h1>Profile Posts Index</h1>
+}
---
+++ src/components/ProfileSection/ProfileSection.js
@@ -0,0 +1,7 @@
+import ProfilePostsIndex from "../ProfilePostsIndex/ProfilePostsIndex"
+
+export default function ProfileSection(){
+    return(
+      <ProfilePostsIndex/>
+    )
+}
